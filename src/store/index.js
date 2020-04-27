c3dab90eba590f93b110767f7d4aab405ca08196
bacteria127 <foreverziyu@gmail.com>
2020-04-27 17:16:53 +0800

    store增加登录逻辑

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    isLogin: localStorage.getItem('isLogin') || '0' // 只有1为已登录
+  },
+  mutations: {
+    login(state) {
+      state.isLogin = '1'
+      localStorage.setItem('isLogin', '1')
+    },
+    loginOut(state) {
+      state.isLogin = '0'
+      localStorage.removeItem('token')
+      localStorage.removeItem('isLogin')
+    }
+  },
+  actions: {}
+})
